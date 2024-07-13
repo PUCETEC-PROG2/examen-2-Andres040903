17c34614d3e4f0a04c877b0306a3e7ef6f075c0f
Andres040903 <eacaillagua@puce.edu.ec>
2024-07-13 10:52:09 -0500

    Examen Actualizado

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    genre = models.CharField(max_length=100)
+    director = models.CharField(max_length=100)
+    year = models.PositiveIntegerField()
+    synopsis = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+
+from django.shortcuts import get_object_or_404
+
+def movie_detail(request, movie_id):
+    movie = get_object_or_404(movie, id=movie_id)
+    return render(request, 'movies/detail.html', {'movie': movie})
